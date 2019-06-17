edca5c2d7da8f2b19426c21a758b9a8e427945e1
zhoulifeng <480970024@qq.com>
2019-06-17 17:59:24 +0800

    echarts图表库

---
+++ src/components/Chart/Chart.vue
@@ -0,0 +1,51 @@
+<template>
+  <div ref="chartDom"></div>
+</template>
+
+<script>
+import echarts from "echarts";
+import { addListener, removeListener } from "resize-detector";
+import debounce from "lodash/debounce";
+export default {
+  name: "Chart",
+  props: {
+    option: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data() {
+    return {};
+  },
+  watch: {
+    option(val) {
+      this.chart.setOption(val);
+    }
+  },
+  created() {
+    //防抖
+    this.resize = debounce(this.resize, 300);
+  },
+  mounted() {
+    this.renderChart();
+    addListener(this.$refs.chartDom, this.resize);
+  },
+  beforeDestroy() {
+    removeListener(this.$refs.chartDom, this.resize);
+    this.chart.dispose();
+    this.chart = null;
+  },
+  methods: {
+    resize() {
+      this.chart.resize();
+    },
+    renderChart() {
+      // 基于准备好的dom，初始化echarts实例
+      this.chart = echarts.init(this.$refs.chartDom);
+      this.chart.setOption(this.option);
+    }
+  }
+};
+</script>
+
+<style></style>
--- src/views/Dashboard/Analysis.vue
+++ src/views/Dashboard/Analysis.vue
@@ -1,9 +1,54 @@
 <template>
-  <div>分析页</div>
+  <div>
+    <Chart :option="chartOption" style="height:400px;" />
+  </div>
 </template>
 
 <script>
-export default {};
+import Chart from "./../../components/Chart/Chart";
+import random from "lodash/random";
+export default {
+  name: "Analysis",
+  data() {
+    return {
+      chartOption: {
+        title: {
+          text: "ECharts 入门示例"
+        },
+        tooltip: {},
+        legend: {
+          data: ["销量"]
+        },
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "销量",
+            type: "bar",
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      }
+    };
+  },
+  mounted() {
+    this.interval = setInterval(() => {
+      this.chartOption.series[0].data = this.chartOption.series[0].data.map(
+        () => {
+          random(100);
+        }
+      );
+    }, 300);
+  },
+  components: {
+    Chart
+  },
+  beforeDestroy() {
+    clearInterval(this.interval);
+  }
+};
 </script>
 
 <style></style>
